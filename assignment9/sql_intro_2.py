141a0c9cff34576a3507f7d17276dafa284f4e62
sleepypanda25 <125111906+sleepypanda25@users.noreply.github.com>
2026-08-28 15:43:22 -0700

    assignment9

--- assignment9/sql_intro_2.py
+++ assignment9/sql_intro_2.py
@@ -3,13 +3,22 @@
 import pandas as pd
 import sqlite3
 
-with sqlite3.connect("../db/magazines.db") as conn:
+with sqlite3.connect("../db/lesson.db") as conn:
     sql_statement = """
-    SELECT s.name, m.magazine_name, p.publisher_name
-    FROM subscriptions sub
-    JOIN subscribers s ON sub.subscriber_id = s.subscriber_id
-    JOIN magazines m ON sub.magazine_id = m.magazine_id
-    JOIN publishers p ON m.publisher_id = p.publisher_id
+    SELECT li.line_item_id, li.quantity, p.product_id, p.product_name, p.price
+    FROM line_items li
+    JOIN products p ON li.product_id = p.product_id
     """
+
     df = pd.read_sql_query(sql_statement, conn)
-    print(df)
+    print(df.head())
+
+    df['total'] = df['quantity'] * df['price']
+
+    print(df.head())
+
+    aggregated_df = df.groupby('product_id').agg({'line_item_id': 'count', 'total': 'sum', 'product_name': 'first'})
+    print(aggregated_df.head())
+
+    df.sort_values(by='product_name', inplace=True)
+    df.to_csv('../csv/order_summary.csv', index=False)
